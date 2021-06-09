45b24e5fb58490754fe20d8873cf035184bf3846
cuongvm <cuongvm@newwave.vn>
2021-06-09 11:44:29 +0700

    refresh token #4

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PageRender from "./PageRender";
 import Login from "./pages/login"
 import Home from "./pages/home"
+import Alert from './components/alert/Alert'
+import { useSelector, useDispatch} from 'react-redux'
+import {refreshToken} from './redux/actions/authAction'
 
 function App() {
+  const { auth } = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(refreshToken())
+  },[dispatch])
+
   return (
     <Router>
+      <Alert />
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={auth.token ? Home : Login} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:id" component={PageRender} />
         </div>
--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -1,20 +1,34 @@
+import { GLOBALTYPES } from './globalTypes'
 import { postDataAPI } from "../../utils/fetchData";
-
-export const TYPES = {
-    AUTH: "AUTH",
-};
 
 export const login = (data) => async (dispatch) => {
     try {
-        dispatch({ type: "NOTIFY", payload: { loading: true } });
+        dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
         const res = await postDataAPI("login", data);
         dispatch({
-            type: "AUTH",
+            type: GLOBALTYPES.AUTH,
             payload: { token: res.data.access_token, user: res.data.user },
         });
         localStorage.setItem("firstLogin", true);
-        dispatch({ type: "NOTIFY", payload: { success: res.data.msg } });
+        dispatch({ type: GLOBALTYPES.ALERT, payload: { success: res.data.msg } });
     } catch (err) {
-        dispatch({ type: "NOTIFY", payload: { error: err.response.data.msg } });
+        dispatch({ type: GLOBALTYPES.ALERT, payload: { error: err.response.data.msg } });
+    }
+};
+
+export const refreshToken = () => async (dispatch) => {
+    const firstLogin = localStorage.getItem("firstLogin");
+    if (firstLogin) {
+        dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
+        try {
+            const res = await postDataAPI("refresh_token");
+            dispatch({
+                type: GLOBALTYPES.AUTH,
+                payload: { token: res.data.access_token, user: res.data.user },
+            });
+            dispatch({ type: GLOBALTYPES.ALERT, payload: {} });
+        } catch (err) {
+            dispatch({ type: GLOBALTYPES.ALERT, payload: { error: err.response.data.msg } });
+        }
     }
 };
